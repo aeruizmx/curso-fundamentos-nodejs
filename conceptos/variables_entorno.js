79408397f84f551ad49b7c82d43889db80e8536e
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-09 11:07:42 -0600

    Nodemon y PM2

--- conceptos/variables_entorno.js
+++ conceptos/variables_entorno.js
@@ -3,5 +3,5 @@
 // NOMBRE=ANDRES WEB=aeruiz.com node conceptos/variables_entorno.js
 let nombre = process.env.NOMBRE || 'SIN NOMBRE'
 let web = process.env.WEB || 'No tengo web'
-console.log('Hola ' + nombre)
+console.log('Hola! ' + nombre)
 console.log('Mi web es ' + web)
