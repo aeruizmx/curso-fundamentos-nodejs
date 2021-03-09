bff64ab3dc048323bf2f7c5726c2ba3926b68ae2
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-09 10:54:14 -0600

    Variables de entorno

---
+++ conceptos/variables_entorno.js
@@ -0,0 +1,7 @@
+// En consola se pone 
+// NOMBRE=ANDRES node conceptos/variables_entorno.js
+// NOMBRE=ANDRES WEB=aeruiz.com node conceptos/variables_entorno.js
+let nombre = process.env.NOMBRE || 'SIN NOMBRE'
+let web = process.env.WEB || 'No tengo web'
+console.log('Hola ' + nombre)
+console.log('Mi web es ' + web)
