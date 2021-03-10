c9e6576edcbed1c439d3f70b1a7d28fbece0d4e2
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-09 19:36:50 -0600

    Console

---
+++ modulos/console.js
@@ -0,0 +1,31 @@
+console.log('console.log')
+console.info('console.info')
+console.error('console.error')
+console.warn('console.warn')
+console.log('console.table')
+let tabla = [
+  {
+    nombre:'Andres',
+    edad:32
+  },
+  {
+    nombre:'Jose',
+    edad:200
+  }
+]
+console.table(tabla)
+console.log('console.group:')
+console.group('conversacion')
+console.log('bla')
+console.log('bla')
+console.log('hola')
+console.log('adios')
+console.groupEnd('conversacion')
+console.count('veces')
+console.count('veces')
+console.count('veces')
+console.count('veces')
+console.count('veces')
+console.count('veces')
+console.countReset(1)
+console.count('veces')
