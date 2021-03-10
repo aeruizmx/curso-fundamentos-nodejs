4f0fba987f686b59984fd962296d4fe898384057
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-10 09:26:44 -0600

    Módulos nativos en C++

---
+++ modulos/nativos/build/Release/.deps/Release/obj.target/addon/hola.o.d
@@ -0,0 +1,15 @@
+cmd_Release/obj.target/addon/hola.o := c++ -o Release/obj.target/addon/hola.o ../hola.cc '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/aruiz/Library/Caches/node-gyp/10.15.1/include/node -I/Users/aruiz/Library/Caches/node-gyp/10.15.1/src -I/Users/aruiz/Library/Caches/node-gyp/10.15.1/deps/openssl/config -I/Users/aruiz/Library/Caches/node-gyp/10.15.1/deps/openssl/openssl/include -I/Users/aruiz/Library/Caches/node-gyp/10.15.1/deps/uv/include -I/Users/aruiz/Library/Caches/node-gyp/10.15.1/deps/zlib -I/Users/aruiz/Library/Caches/node-gyp/10.15.1/deps/v8/include  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/addon/hola.o.d.raw   -c
+Release/obj.target/addon/hola.o: ../hola.cc \
+  /Users/aruiz/Library/Caches/node-gyp/10.15.1/include/node/node.h \
+  /Users/aruiz/Library/Caches/node-gyp/10.15.1/include/node/v8.h \
+  /Users/aruiz/Library/Caches/node-gyp/10.15.1/include/node/v8-version.h \
+  /Users/aruiz/Library/Caches/node-gyp/10.15.1/include/node/v8config.h \
+  /Users/aruiz/Library/Caches/node-gyp/10.15.1/include/node/v8-platform.h \
+  /Users/aruiz/Library/Caches/node-gyp/10.15.1/include/node/node_version.h
+../hola.cc:
+/Users/aruiz/Library/Caches/node-gyp/10.15.1/include/node/node.h:
+/Users/aruiz/Library/Caches/node-gyp/10.15.1/include/node/v8.h:
+/Users/aruiz/Library/Caches/node-gyp/10.15.1/include/node/v8-version.h:
+/Users/aruiz/Library/Caches/node-gyp/10.15.1/include/node/v8config.h:
+/Users/aruiz/Library/Caches/node-gyp/10.15.1/include/node/v8-platform.h:
+/Users/aruiz/Library/Caches/node-gyp/10.15.1/include/node/node_version.h:
