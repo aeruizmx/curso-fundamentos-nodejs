9fdacd19574804df122f32e7220fcfcf07404a75
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-11 09:44:57 -0600

    Buffers

---
+++ memoria/buffer.js
@@ -0,0 +1,15 @@
+// let buffer = Buffer.alloc(4)
+//let buffer = Buffer.from([1,2,7])
+let buffer = Buffer.from('Hola')
+console.log(buffer)
+console.log(buffer.toString())
+
+// --
+
+let abc = Buffer.alloc(26)
+console.log(abc)
+for (let index = 0; index < 26; index++) {
+  abc[index] = index + 97;
+}
+console.log(abc)
+console.log(abc.toString())
